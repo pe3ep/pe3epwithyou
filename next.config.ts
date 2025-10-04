6548eed27b7ce728e41f8997492dfb7c6d771ac4
Andrew <legendarysuwe@gmail.com>
2025-10-05 02:02:27 +0300

    ✨ Add redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      // Social redirects
+      {
+        source: '/github',
+        destination: 'https://github.com/pe3ep',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://x.com/pe3epwithyou',
+        permanent: true,
+      },
+      {
+        source: '/x',
+        destination: 'https://x.com/pe3epwithyou',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/pe3epwithyou/',
+        permanent: true,
+      },
+      {
+        source: '/inst',
+        destination: 'https://www.instagram.com/pe3epwithyou/',
+        permanent: true,
+      },
+      {
+        source: '/twitch',
+        destination: 'https://twitch.tv/pe3epwithyou',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
