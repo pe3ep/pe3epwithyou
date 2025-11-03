addc052cdd2bffd70b5739f7fbdf7a699072e4b3
Andrew <legendarysuwe@gmail.com>
2025-11-03 16:55:35 +0300

    🐛 Fix random facts being refreshed on focus

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -52,8 +52,8 @@ export default function ProjectsPage() {
             This is a list of project's that I've worked on over the past years. This page may include projects that are
             still being worked on
           </p>
-          {/* <EmptyProjects /> */}
-          <ProjectGrid size={[24, 12]} projects={projects} />
+          <EmptyProjects />
+          {/* <ProjectGrid size={[24, 12]} projects={projects} /> */}
         </div>
       </main>
     </div>
@@ -63,7 +63,9 @@ export default function ProjectsPage() {
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const EmptyProjects = () => {
-  const { data, error, isLoading } = useSWR('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en', fetcher)
+  const { data, error, isLoading } = useSWR('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en', fetcher, {
+    revalidateOnFocus: false,
+  })
   return (
     <Empty className="border-brand rounded-2xl border border-dashed">
       <EmptyHeader>
